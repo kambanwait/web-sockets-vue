56506546a54ce7b3f45d3c4cf750480b333058d3
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-02 11:13:12 +0000

    sends a random mumber every 2.5 seconds

--- server/index.js
+++ server/index.js
@@ -9,10 +9,15 @@ server.on('connection', webSocket => {
   console.log('new client connected')
   sockets.push(webSocket)
 
+  const interval = setInterval(() => {
+    console.log('sending new random number')
+    webSocket.send(Math.ceil(Math.random() * 200))
+  }, 2500)
 
   // when socket closes or disconnects, remove it from the array
   webSocket.on('close', () => {
     console.log('close')
+    clearInterval(interval)
     sockets = sockets.filter(s => s !== webSocket)
   })
 
