fa93bf4ec5aeb94e58c9d5ad09ef596ac918898a
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-12-02 15:05:41 +0000

    temp remove interval sending of number

--- server/index.js
+++ server/index.js
@@ -50,15 +50,15 @@ server.on('connection', webSocket => {
   })
 
   // setup an interval to send random number to attached clients
-  const interval = setInterval(() => {
-    console.info('sending new random number')
-    webSocket.send(Math.ceil(Math.random() * 200))
-  }, 2500)
+  // const interval = setInterval(() => {
+  //   console.info('sending new random number')
+  //   webSocket.send(Math.ceil(Math.random() * 200))
+  // }, 2500)
 
   // when socket closes or disconnects, remove it from the array
   webSocket.on('close', () => {
     console.log('close')
-    clearInterval(interval)
+    // clearInterval(interval)
     sockets = sockets.filter(s => s !== webSocket)
   })
 
